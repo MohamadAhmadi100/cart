c9d1b245a05a3ed5e9e1b558552d5d5d077cc1a3
Meysam Taghizadeh <taghizadeh@aasood.com>
2022-02-28 14:32:25 +0000

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,15 @@ class Settings:
     MONGO_USER = os.getenv("MONGO_USER")
     MONGO_PASS = os.getenv("MONGO_PASS")
     MONGO_HOST = os.getenv("MONGO_HOST")
-    MONGO_DB = os.getenv("MONGO_DB")
+    MONGO_PORT = int(os.getenv("MONGO_PORT"))
 
     APP_NAME = os.getenv("APP_NAME")
 
     RABBIT_HOST = os.getenv("RABBIT_HOST")
+    RABBIT_PORT = int(os.getenv("RABBIT_PORT"))
+    RABBIT_USER = os.getenv("RABBIT_PORT")
+    RABBIT_PASS = os.getenv("RABBIT_PORT")
+
 
 
 settings = Settings()
