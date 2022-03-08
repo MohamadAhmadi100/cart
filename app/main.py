7364306dfbe202156c6533d26ed01f14c7b76a5b
Meysam Taghizadeh <taghizadeh@aasood.com>
2022-03-08 11:04:37 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,5 +6,3 @@ if __name__ == '__main__':
     rpc = RabbitRPCClient(receiving_queue=f"{settings.APP_NAME}_googooli", callback=callback,
                           exchange_name="headers_exchange",
                           headers={settings.APP_NAME: True}, headers_match_all=True)
-    rpc.connect()
-    rpc.consume()
