f67fc5d025d33eed3b34bd10e3947d02da8261b9
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-04-18 17:53:16 +0430

    edit cart now inc the count

--- app/models/cart.py
+++ app/models/cart.py
@@ -34,22 +34,27 @@ class Cart:
                 {"user_info.user_id": self.user_info.get('user_id'),
                  "products.system_code": self.product.get('system_code'),
                  "products.storage_id": self.storage_id})
-            if self.count < 1:
-                return Cart.remove_from_cart(self.product.get('system_code'), self.user_info.get('user_id'),
-                                             self.storage_id), "delete"
-            elif db_data:
+
+            if db_data:
+
+                in_cart_count = \
+                    [a for a in db_data.get('products') if a.get("system_code") == self.product.get("system_code")][
+                        0].get("count")
+                if in_cart_count + self.count <= 0:
+                    return self.remove_from_cart(self.product.get('system_code'), self.user_info.get('user_id'),
+                                                 self.storage_id), "delete"
                 result = client.cart_collection.update_one(
                     {"user_info.user_id": self.user_info.get('user_id'),
                      "products": {"$elemMatch": {"system_code": self.product.get('system_code'),
                                                  "storage_id": self.storage_id}}},
-                    {"$set": {"products.$": product}})
+                    {"$inc": {"products.$.count": product['count']}})
             else:
                 result = client.cart_collection.update_one({"user_info.user_id": self.user_info.get('user_id')},
                                                            {'$addToSet': {'products': product}},
                                                            upsert=True)
             if not result.raw_result.get("updatedExisting") or result.modified_count:
-                return "item added to cart successfully"
-            return "nothing changed", "error"
+                return "item edited in cart successfully"
+            return "nothing changed", "nothing"
 
     @staticmethod
     def get_cart(cart_id: int):
